cd48ae507377c35d4fe82cff93a45b730aa92c41
root <root@ns.local.domain>
2016-06-02 05:17:28 -0400

    Initial repository.

---
+++ public_html/index.php
@@ -0,0 +1,144 @@
+<?php
+
+session_start();
+
+class bee {
+
+    var $hitpoints;
+    var $type;
+
+    function getHp() {
+        return $this->hitpoints;
+    }
+
+}
+
+class queen extends bee {
+
+    public $type = 'queen';
+
+    function __construct() {
+
+        $this->hitpoints = 100;
+
+    }
+
+    function hitTheBee() {
+
+        $this->hitpoints = $this->hitpoints-8;
+
+    }
+}
+
+class worker extends bee {
+
+    public $type = 'worker';
+
+    function __construct() {
+        $this->hitpoints = 75;
+    }
+
+    function hitTheBee() {
+        $this->hitpoints = $this->hitpoints - 10;
+    }
+
+}
+
+class drone extends bee {
+
+    public $type = 'drone';
+
+    function __construct() {
+        $this->hitpoints = 50;
+    }
+
+    function hitTheBee() {
+        $this->hitpoints = $this->hitpoints - 12;
+    }
+
+}
+
+if(!isset($_GET['submit'])) {
+    $bees = array(
+        new queen(),
+        new worker(),
+        new worker(),
+        new worker(),
+        new worker(),
+        new worker(),
+        new drone(),
+        new drone(),
+        new drone(),
+        new drone(),
+        new drone(),
+        new drone(),
+        new drone(),
+        new drone(),
+        new drone()
+    );
+    $_SESSION['bees'] = $bees;
+}
+
+
+function hitTheBee() {
+
+    $bees = $_SESSION['bees'];
+
+    if (!$bees) {
+        exit("THE END.");
+    }
+
+    $key = array_rand($bees, 1);
+
+    $hitbee = $bees[$key];
+
+    $hitbee->hitTheBee();
+
+    if ($bees[$key]->getHp() <= 0) {
+        unset($bees[$key]);
+    }
+
+    foreach($bees as $value) {
+        echo $value->type . ": " . $value->getHp() . " HP<br/>";
+    }
+
+    if (isset($_SESSION['hits'])) {
+        echo 'Hits: ' . $_SESSION['hits'];
+    }
+
+    echo '<hr/><a href="/">RESTART</a>';
+
+    $_SESSION['bees'] = $bees;
+
+}
+?>
+    </p>
+
+    <form method="GET">
+        <input type="submit" value="hit-the-bee" name="submit">
+    </form>
+
+<?php
+
+if (isset($_GET['submit'])) {
+
+    $_SESSION['hits'] += 1;
+
+    hitTheBee();
+
+} else {
+
+    $bees = $_SESSION['bees'];
+    $_SESSION['hits'] = 0;
+
+    foreach ($bees as $value) {
+        echo $value->type . ": " . $value->getHp() . " HP<br/>";
+    }
+
+    if ($_SESSION['hits'] > 0) {
+        echo 'Hits: ' . $_SESSION['hits'];
+    }
+
+}
+
+?>
